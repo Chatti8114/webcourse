00b7037e056f8106ade3be7cce48a8fa1e76b969
Chatti8114 <56061719+Chatti8114@users.noreply.github.com>
2019-12-23 11:58:12 +0900

    Delete Test_1.java

--- Test_1.java
+++
@@ -1,9 +0,0 @@
-public class Test_1
-{
-     public static void main(String[] args)
-
-      {
-         System.out.println("æ»≥Á«œººø‰!");
-         System.out.println("π›∞©Ω¿¥œ¥Ÿ.!!!");
-       }
-}
