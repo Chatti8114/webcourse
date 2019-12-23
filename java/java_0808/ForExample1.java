1622bd495e26671d8bf564ea8fd40ce9aa9db1e7
Chatti8114 <56061719+Chatti8114@users.noreply.github.com>
2019-12-23 13:56:53 +0900

    Add files via upload

---
+++ java/java_0808/ForExample1.java
@@ -0,0 +1,12 @@
+package java_0808;
+
+public class ForExample1 {
+	public static void main(String[] args) {
+		
+		for (int i = 0; i <= 4; i++) {
+			System.out.println("0부터 4까지의 숫자 : " + i);
+			
+		}
+	}
+
+}
